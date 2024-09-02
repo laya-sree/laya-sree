85198a5bb5476c2c660c35dd109ac25743770dfc
laya-sree <kolathur.layasree@kalvium.community>
2024-09-02 11:51:19 +0530

    Add files via upload

---
+++ indext.jsx
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <h1> my name is k.layasree</h1>
+        <h2> i want to explore  </h2>
+        <p> i want to be a successful person and secure a good job</p>
+        <img src="OIP.jpg"> </img>
+
+    </body>
+</html>
